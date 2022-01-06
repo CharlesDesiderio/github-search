d6c07747a56ac5fad4d7e8c890a1a85c2f416aa6
Charles Desiderio <cjdesi@gmail.com>
2022-01-05 21:22:00 -0600

    Trying a few things for getting page state to work correctly

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import SearchBar from './components/SearchBar'
 import Results from './components/Results'
 
 const App = () => {
 
-  const [results, setResults] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(0)
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
+  
+  const pageRef = useRef(currentPage)
 
-  const runSearch = (query) => {
-    fetch(`https://api.github.com/search/users?q=${query}`)
+  const runSearch = () => {
+    fetch(`https://api.github.com/search/users?q=${query}&page=${pageRef.current}`)
     .then(res => res.json())
     .then(data => {
       setResults(data.items)
+      console.log('page', currentPage)
       setTotalPages(Math.ceil(data.total_count / 30))
     })
   }
 
+  const decPage = () => {
+    if (currentPage > 1) {
+      pageRef.current = currentPage - 1
+      setCurrentPage(pageRef.current)
+    }
+    runSearch()
+  }
+
+  const incPage = () => {
+    if (currentPage < totalPages) {
+      pageRef.current = currentPage + 1
+      setCurrentPage(pageRef.current)
+    }
+    runSearch()
+    console.log(currentPage)
+  }
+
   return (
     <div>
-      <SearchBar runSearch={runSearch} />
+      <SearchBar runSearch={runSearch} setQuery={setQuery} />
+
       <Results data={results} />
-      {totalPages}
+
+      { totalPages > 0 ? <div><span onClick={decPage}> ◀ </span>Page {currentPage} of {totalPages} <span onClick={incPage}> ▶ </span></div> : '' }
+
     </div>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 
-const SearchBar = ({ runSearch }) => {
+const SearchBar = ({ runSearch, setQuery }) => {
 
 const [searchInput, setSearchInput] = useState('')
 
 const handleChange = (event) => {
   setSearchInput(event.target.value)
+  setQuery(event.target.value)
 }
 
   return (
     <div>
     <input value={searchInput} onChange={handleChange} />
-    <button onClick={() => runSearch(searchInput)}>Search</button>
+    <button onClick={runSearch}>Search</button>
     </div>
   )
 }
